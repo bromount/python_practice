b4a621d342902872f415b7bb9aa416a9ad41bef0
bromount <iambromount@gmail.com>
2018-06-24 20:56:05 +0530

    new files

--- lists.py
+++ lists.py
@@ -1,13 +1,41 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  9 11:26:23 2018
+
+@author: bromount
+"""
+
 names = ["Annamalai","Arunachalam"]
 
 print names
 
-names.append("sinthuja")
+print names[0]
+
+print len(names)
+
+print len(names[0])
+
+names.append("Sinthuja")
 
 print names
 
-#names.extend("harikrishnan")
+print names.pop()
 
-names.insert((1),'1983')
+print names
+
+names.extend(["sinthuja","harikrishnan"])
+
+print names
+
+names.remove("harikrishnan")
+
+print names
+
+names.insert(3,"sowmeya")
+
+print names
+
+names.insert(1,1983)
 
 print names
