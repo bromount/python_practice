dce77fa179bc82e298a54ff4fc34c0ecfea898e0
Annamalai Arunachalam <aa00482762@techmahindra.com>
2018-06-27 19:24:02 +0530

    added some OS commands for ubuntu

---
+++ practice_4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 27 15:46:38 2018
+
+@author: bromount
+"""
+
+import os
+
+os.system("ls -lrt")
+os.system("which java")
+os.chmod("/home/bromount/My Works/python_practice/BMI.py",771)
+os.chdir("/home/bromount/")
+os.system("pwd")
