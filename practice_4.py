36cc5806f67d2d7635f576011519e49139bfe6b2
Annamalai Arunachalam <aa00482762@techmahindra.com>
2018-06-29 11:45:05 +0530

    modified os related commands

--- practice_4.py
+++ practice_4.py
@@ -10,6 +10,10 @@ import os
 
 os.system("ls -lrt")
 os.system("which java")
-os.chmod("/home/bromount/My Works/python_practice/BMI.py",771)
+os.chmod("/home/bromount/Documents/duplicate_songs.txt",771)
+os.system("ls -lrt /home/bromount/Documents/duplicate_songs.txt")
 os.chdir("/home/bromount/")
 os.system("pwd")
+os.system("df -h")
+os.system("google-chrome")
+os.system("firefox")
