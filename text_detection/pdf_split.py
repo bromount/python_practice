d45f558ac5c7ea8248427641ee93a08f898f4568
Annamalai Arunachalam <aa00482762@techmahindra.com>
2018-08-02 13:14:38 +0530

    modifying the image file and split

--- text_detection/pdf_split.py
+++ text_detection/pdf_split.py
@@ -8,6 +8,8 @@ Created on Wed Aug  1 10:35:19 2018
 
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
+#Getting Input from user
+
 doc_path=raw_input("Enter the file path with file name : ")
 
 inputpdf = PdfFileReader(open(doc_path, "rb"))
--- text_detection/pdf_to_image.py
+++ text_detection/pdf_to_image.py
@@ -6,10 +6,12 @@ Created on Wed Aug  1 13:28:42 2018
 @author: bromount
 """
 from wand.image import Image
+
 # Converting first page into JPG
 with Image(filename="document-page1.pdf") as img:
-     img.save(filename="/temp.jpg")
-# Resizing this image
-#with Image(filename="/temp.jpg") as img:
- #    img.resize(200, 150)
-  #   img.save(filename="/thumbnail_resize.jpg")
+     img.save(filename="temp.jpg")
+     
+# Resizing this image we can use for Book Title
+'''with Image(filename="/temp.jpg") as img:
+     img.resize(200, 150)
+     img.save(filename="/thumbnail_resize.jpg")'''
