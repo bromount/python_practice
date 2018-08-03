52111c9a065429a8971139e5e8958fe688690097
Annamalai Arunachalam <aa00482762@techmahindra.com>
2018-08-03 08:23:00 +0530

    merged to single file and new test

--- text_detection/pdf_split.py
+++ text_detection/pdf_split.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Aug  1 10:35:19 2018
-
+Modified on Fri Aug 3 08:20:10 2018
 @author: bromount
 """
 
 from PyPDF2 import PdfFileWriter, PdfFileReader
+from wand.image import Image
 
 #Getting Input from user
 
@@ -17,5 +18,10 @@ inputpdf = PdfFileReader(open(doc_path, "rb"))
 for i in range(inputpdf.numPages):
     output = PdfFileWriter()
     output.addPage(inputpdf.getPage(i))
-    with open("document-page%s.pdf" % i, "wb") as outputStream:
-        output.write(outputStream)  
+    with open("./pdf/document-page%s.pdf" % i, "wb") as outputStream:
+        output.write(outputStream)
+        print "Page %s saved" % i
+        # Converting pages into JPG
+    with Image(filename="./pdf/document-page%s.pdf" % i) as img:
+        img.save(filename="./pdf/pdf_image%s.jpg" % i)
+        print "Image %s saved" % i
